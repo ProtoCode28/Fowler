205a0ed5f07bf3d5dd136a7bd5048e50a5735736
ProtoCode28 <maier.maximilian2@student.dhbw-karlsruhe.de>
2020-05-14 17:29:02 +0200

    Moved get charge to the class Movie

--- src/Rental.java
+++ src/Rental.java
@@ -17,20 +17,6 @@ class Rental {
     }
 
     double getCharge() {
-        double result = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR -> {
-                result += 2;
-                if (getDaysRented() > 2)
-                    result += (getDaysRented() - 2) * 1.5;
-            }
-            case Movie.NEW_RELEASE -> result += getDaysRented() * 3;
-            case Movie.CHILDRENS -> {
-                result += 1.5;
-                if (getDaysRented() > 3)
-                    result += (getDaysRented() - 3) * 1.5;
-            }
-        }
-        return result;
+        return this.movie.getCharge(this.daysRented);
     }
 }
